36bdecbd3500bf11a273369b060736dd404ebcfe
yanaiara <yanaiara@alunos.utfpr.edu.br>
2021-01-26 19:38:45 -0300

    implementada rota evidenciando o id

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -9,6 +9,17 @@ class PessoaController {
       return res.status(500).json(error.message);
     }
   }
+  static async getOnePeople(req, res) {
+    const { id } = req.params;
+    try {
+      const onePeople = await database.Pessoas.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(onePeople);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
 }
 
 module.exports = PessoaController;
