cf2c2725637efd6dbea11d7d566bd0833e783598
yanaiara <yanaiara@alunos.utfpr.edu.br>
2021-01-26 20:04:24 -0300

    CRUD COMPLETO

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -20,6 +20,37 @@ class PessoaController {
       return res.status(500).json(error.message);
     }
   }
+  static async createPeople(req, res) {
+    const newPeople = req.body;
+    try {
+      const newPeopleCreated = await database.Pessoas.create(newPeople);
+      return res.status(200).json(newPeopleCreated);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+  static async modificPeople(req, res) {
+    const { id } = req.params;
+    const newInfos = req.body;
+    try {
+      await database.Pessoas.update(newInfos, { where: { id: Number(id) } });
+      const atualizedPeople = await database.Pessoas.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).send(atualizedPeople);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+  static async deletePeople(req, res) {
+    const { id } = req.params;
+    try {
+      await database.Pessoas.destroy({ where: { id: Number(id) } });
+      return res.status(200).send({message: "Id deletado com sucesso!"})
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
 }
 
 module.exports = PessoaController;
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -5,5 +5,7 @@ const router = Router();
 
 router.get("/pessoas", PessoaController.getPessoas);
 router.get("/pessoas/:id", PessoaController.getOnePeople);
-
+router.post("/pessoas", PessoaController.createPeople);
+router.put("/pessoas/:id", PessoaController.modificPeople);
+router.delete("/pessoas/:id", PessoaController.deletePeople);
 module.exports = router;
